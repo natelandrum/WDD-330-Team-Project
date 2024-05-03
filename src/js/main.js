3887bd8cb5532247b09c60747b9ca9ca116bf0de
VinoliaMoekwa <moe23001@byui.edu>
2024-05-03 22:11:22 +0200

    fix cart

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-import ProductData from '/ProductData.mjs';
-
-const productDataInstance = new ProductData();
-
-import ProductListing from './ProductList.mjs';
-
-const productListingInstance = new ProductListing();
