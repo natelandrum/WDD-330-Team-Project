96e9a71be0c3ecad30e21e3529f4d3a6c55b81eb
VinoliaMoekwa <122517235+VinoliaMoekwa@users.noreply.github.com>
2024-05-09 23:00:09 +0200

    Delete src/js/productSearch.mjs
    removed

--- src/js/productSearch.mjs
+++
@@ -1,5 +0,0 @@
-const queryString = window.location.search;
-const urlParams = new urlParams(queryString);
-const searchQuery = urlParams.get("search");
-
-document.getElementById("productSearch").innerHTML = "<p>Showing results : ${searchQuery}</p>";
